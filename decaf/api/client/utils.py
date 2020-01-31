dff161f3ea62e29ad5a752be5a17ba9c6c659e1e
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2020-02-01 07:52:58 +0800

    feat(utils): add some functions and definitions for convenience
    These are meant to be useful on import-side.

--- decaf/api/client/utils.py
+++ decaf/api/client/utils.py
@@ -1,20 +1,182 @@
-__all__ = ["make_lookup_table"]
+"""
+This module provides data, type and function definitions which could be useful on import-side. Some of there are used
+internally in this library, too.
+"""
 
-from typing import Dict, Iterable, TypeVar
+__all__ = [
+    "WithId",
+    "identity",
+    "make_lookup_table",
+    "parse_isodate",
+    "percentify",
+    "remove_microseconds",
+    "skipnone",
+]
+
+from decimal import Decimal
+from typing import Callable, Dict, Iterable, Optional, TypeVar, Union
 from typing_extensions import Protocol
 
-#: Defines a type alias for resource identifiers.
-_Id = TypeVar("_Id")
+from .types import Date, DateTime
+
+#: Defines a generic type alias.
+_T = TypeVar("_T")
 
 
-class WithId(Protocol[_Id]):
+def identity(x: _T) -> _T:
+    """
+    Provides an identity function.
+
+    Return value type is inferred the same as the parameter type.
+
+    :param x: Value.
+    :return: Value.
+
+    >>> identity(1)
+    1
+    >>> identity("value")
+    'value'
+    """
+    return x
+
+
+def skipnone(func: Callable[[_T], _T]) -> Callable[[Optional[_T]], Optional[_T]]:
+    """
+    Provides a function decorator for avoiding function application if the value is ``None``.
+
+    :param func: Function to decorate.
+    :return: Decorated function
+
+    >>> add1: Callable[[int], int] = lambda x: x + 1
+    >>> add1(1)
+    2
+    >>> add1(None)
+    Traceback (most recent call last):
+    ...
+    TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
+    >>> skipnone(add1)(1)
+    2
+    >>> skipnone(add1)(None)
+    >>>
+
+    You can define a new function using the decorator (note the function signature):
+
+    >>> safe_add1: Callable[[Optional[int]], Optional[int]] = skipnone(add1)
+    >>> safe_add1(1)
+    2
+    >>> safe_add1(None)
+    >>>
+
+    ... or decorate functions as such:
+
+    >>> @skipnone
+    ... def double(x: int) -> int:
+    ...     return x * 2
+    >>> double(1)
+    2
+    >>> double(None)
+    >>>
+    """
+    return lambda x: x if x is None else func(x)
+
+
+def remove_microseconds(x: DateTime) -> DateTime:
+    """
+    Removes microseconds from the given :py:class:`datetime.datetime` instance.
+
+    :param x: :py:class:`datetime.datetime` instance
+    :return: A new :py:class:`datetime.datetime` instance with microseconds set to ``0``.
+
+    >>> DateTime(2019, 1, 2, 3, 4, 5, 6789)
+    datetime.datetime(2019, 1, 2, 3, 4, 5, 6789)
+    >>> remove_microseconds(DateTime(2019, 1, 2, 3, 4, 5, 6789))
+    datetime.datetime(2019, 1, 2, 3, 4, 5)
+    """
+    return x.replace(microsecond=0)
+
+
+def percentify(x: Union[float, int, str, Decimal]) -> Decimal:
+    """
+    Consumes a value representing a number and returns its percentage points as a :py:class:`Decimal` instance.
+
+    :param x: A value representing a number.
+    :return: Percentage points as a :py:class:`Decimal` instance.
+    :raises decimal.InvalidOperation: In case that parameter can not be converted to valid :py:class:`Decimal` instance.
+
+    >>> percentify(0.01)
+    Decimal('1.00')
+    >>> percentify(0)
+    Decimal('0')
+    >>> percentify("0.01")
+    Decimal('1.00')
+    >>> percentify(Decimal("0.01"))
+    Decimal('1.00')
+    >>> percentify(1.0)
+    Decimal('100.0')
+    >>> percentify(1)
+    Decimal('100')
+    >>> percentify("1")
+    Decimal('100')
+    >>> percentify(Decimal(1))
+    Decimal('100')
+    >>> percentify("xyz")
+    Traceback (most recent call last):
+    ...
+    decimal.InvalidOperation: [<class 'decimal.ConversionSyntax'>]
+    """
+    return Decimal(str(x)) * Decimal(100)
+
+
+def parse_isodate(x: str) -> Date:
+    """
+    Attempts to parse an ISO formatted date string and returns it as a :py:class:`datetime.date` instance.
+
+    :param x: ISO formatted date string
+    :return: A :py:class:`datetime.date` instance, if parsing is successful.
+    :raises ValueError: If the parameter does not represent a valid date or is not formatted as a valid ISO date string.
+
+    >>> parse_isodate("2019-01-02")
+    datetime.date(2019, 1, 2)
+    >>> parse_isodate("2019-02-29")
+    Traceback (most recent call last):
+    ...
+    ValueError: day is out of range for month
+    >>> parse_isodate("20190228")
+    Traceback (most recent call last):
+    ...
+    ValueError: time data '20190228' does not match format '%Y-%m-%d'
+    """
+    return DateTime.strptime(x, "%Y-%m-%d").date()
+
+
+class WithId(Protocol[_T]):
     """
     Provides a generic protocol for models with ``id`` attribute.
     """
 
     #: Unique identifier of the instance.
-    id: _Id
+    id: _T
 
 
-def make_lookup_table(iterable: Iterable[WithId[_Id]]) -> Dict[_Id, WithId[_Id]]:
-    return {r.id: r for r in iterable}
+def make_lookup_table(records: Iterable[WithId[_T]]) -> Dict[_T, WithId[_T]]:
+    """
+    Creates a lookup table from a iterable of records following :py:class:`WithId` protocol.
+
+    :param records: Iterable of records.
+    :return: A lookup table of record id to record.
+
+    >>> make_lookup_table([])
+    {}
+    >>> from dataclasses import dataclass
+    >>> @dataclass
+    ... class A:
+    ...     id: int
+    ...     value: bool
+    >>> records = [A(1, True), A(2, False)]
+    >>> table = make_lookup_table(records)
+    >>> table[1].value
+    True
+    >>> table[2].value
+    False
+    """
+    return {r.id: r for r in records}
