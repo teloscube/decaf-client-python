8a769ccd890ab6061cf4c597b5b3911f7e78dce3
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2021-10-11 14:08:42 +0800

    feat: add py38 and py39 to supported versions

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     author="Vehbi Sinan Tunalioglu",
     author_email="vst@vsthost.com",
